2d32d38abd437d00c0629a69cba20ac5ca21036c
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-18 14:46:14 +0700

    Edit Header.js to display different text

--- my-app/components/Header.js
+++ my-app/components/Header.js
@@ -7,15 +7,14 @@ export default function Header({ noteCount }) {
   return (
     <header className="flex justify-between items-center py-4 border-b dark:border-gray-700">
       <div className="flex items-center gap-2">
-        <span className="text-xl font-bold">📄 Ghi Chú Cá Nhân</span>
+        <span className="text-xl font-bold">📄 Giấy ghi chú ước nguyện 来听下王欣宇甜甜的</span>
       </div>
       <div className="flex items-center gap-4">
-        {/* Nhận props và hiển thị */}
+
         <span className="bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-200 px-4 py-1 rounded-full text-sm font-semibold">
           {noteCount} ghi chú
         </span>
 
-        {/* Nút Toggle Theme */}
         <button
           onClick={toggleTheme}
           className="p-2 rounded-full bg-gray-200 dark:bg-[#2A2B36] hover:bg-gray-300 transition"
